c755068681e89f50086a7d487c46df15b7ad8583
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-12-04 12:36:54 -0800

    correct install using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,16 @@
 # using pip3 to install flask
-exec {'flask':
-    command => 'pip3 install Flask==2.1.0',
-    path    => '/usr/bin'
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/flask --version | grep "Flask 2.1.0"',
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show werkzeug | grep "Version: 2.1.1"',
 }
