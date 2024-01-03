09aa2cb5d56396819ad3fbcb27c40dfba41a6e59
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-01-03 05:07:17 +0300

    correct 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,6 +2,7 @@
 """
 This module uses a REST API to get information about a given employee's TODO
 """
+import re
 import requests
 import sys
 
@@ -12,44 +13,31 @@ def fetch_employee_todo_progress(employee_id):
     """
     todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
         employee_id)
-    users_url = "https://jsonplaceholder.typicode.com/users"
 
-    try:
-        # Fetch todos data
-        todos_response = requests.get(todos_url)
-        todos = todos_response.json()
+    # Fetch todos data
+    todos_response = requests.get(todos_url)
+    todos = todos_response.json()
 
-        # Fetch user data
-        users_response = requests.get(users_url)
-        users = users_response.json()
+    # Fetch user data for the specific employee ID
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id)
+    user_response = requests.get(user_url)
+    user = user_response.json()
 
-        # Find the user by ID
-        user = None
-        for user in users:
-            if user["id"] == int(employee_id):
-                break
+    if 'id' in user:
+        employee_name = user['name']
 
-        if user:
-            employee_name = user['name']
+        completed_tasks = [task for task in todos if task['completed']]
+        total_tasks = len(todos)
+        num_completed_tasks = len(completed_tasks)
 
-            completed_tasks = [task for task in todos if task['completed']]
-            total_tasks = len(todos)
-            num_completed_tasks = len(completed_tasks)
-
-            print("Employee {} is done with tasks({}/{}):".format(
-                employee_name, num_completed_tasks, total_tasks))
-            for task in completed_tasks:
-                print(f"\t {task['title']}")
-        else:
-            print("Employee ID not found")
-    except requests.RequestException as e:
-        print(f"Request failed: {e}")
+        print("Employee {} is done with tasks({}/{}):".format(
+            employee_name, num_completed_tasks, total_tasks))
+        for task in completed_tasks:
+            print(f"\t {task['title']}")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-    fetch_employee_todo_progress(employee_id)
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            fetch_employee_todo_progress(sys.argv[1])
