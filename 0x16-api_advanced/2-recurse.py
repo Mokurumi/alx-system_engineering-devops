ad9da6a14056a0aad915dd6bfd59926d0d38163a
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-01-19 16:22:51 +0300

    retry2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -12,7 +12,7 @@ def recurse(subreddit, hot_list=[], after=None):
     articles for a given subreddit
     """
     url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
-                                                                  after)
+                                                                 after)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                              'AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Chrome/81.0.4044.138 Safari/537.36'}
