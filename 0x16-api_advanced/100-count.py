9f1915ee45acaaf998e293d0dac319c872bc27b3
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-01-19 16:21:45 +0300

    100 retry

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -13,7 +13,7 @@ def count_words(subreddit, word_list, after=None, word_dict={}):
     Javascript should count as javascript, but java should not)
     """
     url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
-                                                                  after)
+                                                                 after)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                              'AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Chrome/81.0.4044.138 Safari/537.36'}
