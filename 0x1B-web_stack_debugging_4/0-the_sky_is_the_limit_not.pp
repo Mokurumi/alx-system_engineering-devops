faed530d87c96b942c8674dfdeb46ffab984e0aa
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-03-15 02:06:40 +0300

    debugging 4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# File: 0-the_sky_is_the_limit.pp
+
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
